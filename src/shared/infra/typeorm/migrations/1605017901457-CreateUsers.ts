7eb134ffb99a6e3cb3511dbd4eba75cd794678bc
Cesar Emmanuel <cesaremmmanul@gmail.com>
2021-02-18 10:29:24 -0300

    Fixing the migration

--- src/shared/infra/typeorm/migrations/1605017901457-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1605017901457-CreateUsers.ts
@@ -2,6 +2,8 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export default class CreateUsers1605017901457 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+
         await queryRunner.createTable(
             new Table({
                 name: 'users',
